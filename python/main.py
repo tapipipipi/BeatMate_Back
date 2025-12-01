3c3dadd2467a3fe79dcadb4e52242e61303b70a8
tapipipipi <y.tani.1226@gmail.com>
2025-12-01 10:47:04 +0900

    fastAPIの初期環境構築

--- python/main.py
+++ python/main.py
@@ -1,1 +1,13 @@
-print("hello world")
+from fastapi import FastAPI, Response
+import uvicorn
+
+app = FastAPI()
+
+#python起動コマンド：uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+
+@app.post("/")
+def root():
+    return {"hello beatmate_python"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8081, log_level="debug")
